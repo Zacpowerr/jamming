07d8afae8980ac00adeef7cfb8baf63254073a57
Zacpowerr <e_kempfer@hotmail.com>
2025-03-16 10:39:42 -0600

    Add TrackList component to display and manage a list of tracks

---
+++ src/components/Tracklist.jsx
@@ -0,0 +1,26 @@
+import Track from "./Track";
+
+function TrackList({ trackList, removeTrackFromTrackList }) {
+  return (
+    <div className="tracklist">
+      <h2>Track List</h2>
+      {trackList.map((track) => (
+        <div key={`track-${track.id}`}>
+          <div key={track.id} className="track-item">
+            <Track key={track.id} track={track} />
+            <button
+              key={`remove-${track.id}`}
+              className="remove-button"
+              onClick={() => removeTrackFromTrackList(track)}
+            >
+              -
+            </button>
+          </div>
+          <div key={`divider-${track.id}`} className="divider-h"></div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default TrackList;
