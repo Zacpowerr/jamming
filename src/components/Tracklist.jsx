5055b3fbd6d666b114cf71d606ed3c6b9c397c5b
Zacpowerr <e_kempfer@hotmail.com>
2025-03-19 19:23:23 -0600

    Add divider between tracks in TrackList component for improved layout

--- src/components/Tracklist.jsx
+++ src/components/Tracklist.jsx
@@ -4,21 +4,22 @@ function TrackList({ trackList, removeTrackFromTrackList }) {
   return (
     <div className="tracklist">
       <h2>Track List</h2>
-      {trackList.map((track) => (
-        <div key={`track-${track.id}`}>
-          <div key={track.id} className="track-item">
-            <Track key={track.id} track={track} />
-            <button
-              key={`remove-${track.id}`}
-              className="remove-button"
-              onClick={() => removeTrackFromTrackList(track)}
-            >
-              -
-            </button>
+      {trackList.length > 0 &&
+        trackList.map((track) => (
+          <div key={`track-${track.id}`}>
+            <div key={track.id} className="track-item">
+              <Track key={track.id} track={track} />
+              <button
+                key={`remove-${track.id}`}
+                className="remove-button"
+                onClick={() => removeTrackFromTrackList(track)}
+              >
+                -
+              </button>
+            </div>
+            <div key={`divider-${track.id}`} className="divider-h"></div>
           </div>
-          <div key={`divider-${track.id}`} className="divider-h"></div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
